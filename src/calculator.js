2625d30e54716c1a80b55e1e9830a6492e270a2c
razmiO <thomas.cuesta31@gmail.com>
2024-12-10 15:51:35 +0100

    feat : add fibo

--- src/calculator.js
+++ src/calculator.js
@@ -11,3 +11,8 @@ export const isOdd = (a) => a % 2 === 1;
 export const isEven = (a) => a % 2 === 0;
 
 export const isFactorOf = (a, b) => b % a === 0;
+
+export const fibonnaci = (n) => {
+  if (n <= 1) return n;
+  return fibonnaci(n - 1) + fibonnaci(n - 2);
+};
