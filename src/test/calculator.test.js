9059a49c91061663dedce919d17b7f5fa29942db
razmiO <thomas.cuesta31@gmail.com>
2024-12-10 14:55:47 +0100

    refactor : calculator.test.js is now using one suite

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -1,66 +1,30 @@
 import { sum, multiplication, division, modulo, substract, isEven, isOdd, isFactorOf } from "../calculator.js";
 import { describe, it } from "node:test";
+import { strictEqual } from "node:assert";
 
-describe("Correct sum", () => {
-  it("adds 1 + 2 to equal 3", () => {
-    if (sum(1, 2) !== 3) {
-      throw new Error("1 + 2 did not equal 3");
-    }
-  });
-});
-
-describe("Correct multiplication", () => {
-  it("multiplies 2 * 3 === 6", () => {
-    if (multiplication(2, 3) !== 6) {
-      throw new Error("2 * 3 did not equal 6");
-    }
-  });
-});
-
-describe("Correct division", () => {
-  it("divides 10 / 2 === 5", () => {
-    if (division(10, 2) !== 5) {
-      throw new Error("10 / 2 did not equal 5");
-    }
-  });
-});
-
-describe("Correct modulo", () => {
-  it("modulos 10 % 3 === 1", () => {
-    if (modulo(10, 3) !== 1) {
-      throw new Error("10 % 3 did not equal 1");
-    }
-  });
-});
-
-describe("Correct substract", () => {
-  it("substracts 10 - 3 === 7", () => {
-    if (substract(10, 3) !== 7) {
-      throw new Error("10 - 3 did not equal 7");
-    }
-  });
-});
-
-describe("Correct isOdd", () => {
-  it("checks if 3 is odd", () => {
-    if (!isOdd(3)) {
-      throw new Error("3 is odd");
-    }
-  });
-});
-
-describe("Correct isEven", () => {
-  it("checks if 4 is even", () => {
-    if (!isEven(4)) {
-      throw new Error("4 is even");
-    }
-  });
-});
-
-describe("Correct isFactorOf", () => {
-  it("checks if 3 is a factor of 6", () => {
-    if (!isFactorOf(3, 6)) {
-      throw new Error("3 is a factor of 6");
-    }
-  });
+describe("Calculator function correctly", () => {
+  // SUM
+  // --
+  it("SUM : adds 1 + 2 to equal 3", () => strictEqual(sum(1, 2), 3));
+  // MULTIPLICATION
+  // --
+  it("MULTIPLICATION : multiplies 2 * 3 === 6", () => strictEqual(multiplication(2, 3), 6));
+  // DIVISION
+  // --
+  it("DIVISION : divides 10 / 2 === 5", () => strictEqual(division(10, 2), 5));
+  // MODULO
+  // --
+  it("MODULO : modulos 10 % 3 === 1", () => strictEqual(modulo(10, 3), 1));
+  // SUBSTRACTION
+  // --
+  it("SUBSTRACTION : substracts 10 - 3 === 7", () => strictEqual(substract(10, 3), 7));
+  // ODD
+  // --
+  it("ODD : checks if 3 is odd", () => strictEqual(isOdd(3), true));
+  // EVEN
+  // --
+  it("EVEN : checks if 4 is even", () => strictEqual(isEven(4), true));
+  // FACTOR OF
+  // --
+  it("FACTOR OF : checks if 3 is a factor of 6", () => strictEqual(isFactorOf(3, 6), true));
 });
