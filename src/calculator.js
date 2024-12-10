5f45a3961bad6ace36d70d00bf2130709fc2994f
razmiO <thomas.cuesta31@gmail.com>
2024-12-10 11:34:43 +0100

    Add modulo function to calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,5 @@
 export const sum = (a, b) => a + b;
 export const multiplication = (a, b) => a * b;
 export const division = (a, b) => a / b;
+
+export const modulo = (a, b) => a % b;
--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -1,26 +1,34 @@
-import { sum, multiplication, division } from '../calculator.js';
-import { describe, it } from 'node:test';
+import { sum, multiplication, division, modulo } from "../calculator.js";
+import { describe, it } from "node:test";
 
-describe('Correct sum', () => {
-    it('adds 1 + 2 to equal 3', () => {
-        if (sum(1, 2) !== 3) {
-            throw new Error('1 + 2 did not equal 3');
-        }
-    });
+describe("Correct sum", () => {
+  it("adds 1 + 2 to equal 3", () => {
+    if (sum(1, 2) !== 3) {
+      throw new Error("1 + 2 did not equal 3");
+    }
+  });
 });
 
-describe('Correct multiplication', () => {
-    it('multiplies 2 * 3 === 6', () => {
-        if (multiplication(2, 3) !== 6) {
-            throw new Error('2 * 3 did not equal 6');
-        }
-    });
+describe("Correct multiplication", () => {
+  it("multiplies 2 * 3 === 6", () => {
+    if (multiplication(2, 3) !== 6) {
+      throw new Error("2 * 3 did not equal 6");
+    }
+  });
 });
 
-describe('Correct division', () => {
-    it('divides 10 / 2 === 5', () => {
-        if (division(10, 2) !== 5) {
-            throw new Error('10 / 2 did not equal 5');
-        }
-    });
+describe("Correct division", () => {
+  it("divides 10 / 2 === 5", () => {
+    if (division(10, 2) !== 5) {
+      throw new Error("10 / 2 did not equal 5");
+    }
+  });
+});
+
+describe("Correct modulo", () => {
+  it("modulos 10 % 3 === 1", () => {
+    if (modulo(10, 3) !== 1) {
+      throw new Error("10 % 3 did not equal 1");
+    }
+  });
 });
